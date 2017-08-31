1b3b2f8b5393816dcd70e6f390cbdf274f26dfca
leaveyourcave <leaveyourcave@gmail.com>
2017-08-31 10:10:30 +0200

    Initial commit - simple Tic Tac Toe environment

---
+++ q_table/GameEnvironment.py
@@ -0,0 +1,106 @@
+import numpy as np
+
+
+class GameEnvironment:
+    def __init__(self, size):
+        self.size = size
+        self.board = np.chararray((self.size, self.size))
+        self.reset()
+
+    def reset(self):
+        self.board[:] = '.'
+
+    def check_game_state(self):
+        # check horizontal
+        for y in range(0, self.size):
+            all_ok = True
+            prev = self.board[y, 0]
+            for x in range(1, self.size):
+                if prev == '.' or self.board[y, x] != prev:
+                    all_ok = False
+
+            if all_ok:
+                return prev  # WINNER
+
+        # check vertical
+        for x in range(0, self.size):
+            all_ok = True
+            prev = self.board[0, x]
+            for y in range(1, self.size):
+                if prev == '.' or self.board[y, x] != prev:
+                    all_ok = False
+
+            if all_ok:
+                return prev  # WINNER
+
+        # check diagonal, TODO add checking second diagonal
+        prev = self.board[0, 0]
+        all_ok = True
+        for xy in range(1, self.size):
+            if prev == '.' or self.board[xy, xy] != prev:
+                all_ok = False
+
+        if all_ok:
+            return prev  # WINNER
+
+        # check if draw
+        all_marked = True
+        for x in range(0, self.size):
+            for y in range(0, self.size):
+                if self.board[y, x] != '.':
+                    all_marked = False
+
+        if all_marked:
+            return '.'  # DRAW
+
+        return None
+
+    def make_computer_move(self):
+        available_indexes = np.where(self.board == '.')
+        random_choice = np.random.randint(0, available_indexes[0].size)
+        rand_y = available_indexes[0][random_choice]
+        rand_x = available_indexes[1][random_choice]
+        self.board[rand_y, rand_x] = 'o'
+
+    def step(self, action):
+        y_coordinate = action[0]
+        x_coordinate = action[1]
+
+        if y_coordinate >= self.size or x_coordinate >= self.size:
+            # Wrong move - field doesn't exist
+            return -3
+
+        if self.board[y_coordinate, x_coordinate] != '.':
+            # Wrong move - field already taken
+            return -3
+
+        self.board[y_coordinate, x_coordinate] = 'x'
+
+        state = self.check_game_state()
+        if state is None:
+            # if not end, make COM move
+            self.make_computer_move()
+            # checkGameState and return reward
+            state = self.check_game_state()
+
+        # Interpret the state
+        if state is None:
+            return 0
+        if state == '.':
+            return 1
+        elif state == 'x':
+            return 2
+        elif state == 'o':
+            return -1
+        else:
+            return 0  # shouldn't happen
+
+
+d = GameEnvironment(3)
+print(d.board)
+print(d.step((1, 1)))
+print(d.board)
+print(d.step((1, 2)))
+print(d.board)
+print(d.step((1, 0)))
+print(d.board)
---
+++ q_table/Learn.py
@@ -0,0 +1,13 @@
+from q_table.GameEnvironment import GameEnvironment
+
+game_size = 3
+
+####
+#  Q table
+# - row: Game state -
+# - column: Action - tuple
+# - value: reward value
+#
+q =
+
+env = GameEnvironment(game_size)
